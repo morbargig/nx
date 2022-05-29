a86164badd4c4dcfe60d4749f8088a2421003162
mor bargig <mor@ideo-digital.com>
2022-05-29 20:12:59 +0300

    before story-book

--- libs/front/dynamic-forms/src/lib/forms/core/services/dynamic-form-builder.service.ts
+++ libs/front/dynamic-forms/src/lib/forms/core/services/dynamic-form-builder.service.ts
@@ -7,6 +7,9 @@ import {
 } from '@angular/forms';
 import { DynamicFormControl } from '../interfaces/field-config';
 import { FormArray } from '@angular/forms';
+import { JsonObject, JsonValue } from '@fnx-nx/api-interfaces';
+import { FormArrayData } from '../../form-fields/form-array/form-array';
+import { FormGroupData } from '../../form-fields/form-group/form-group';
 
 @Injectable({
   providedIn: 'root',
@@ -102,14 +105,12 @@ export class DynamicFormBuilderService {
             const configs: DynamicFormControl[] = <DynamicFormControl[]>(
               state.data?.formGroupConfig
             );
-            return this.fb.array(
-              [
-                this.recursionBuildGroup(configs, {
-                  validators: state?.data?.groupValidations || [],
-                  asyncValidators: state?.data?.asyncGroupValidations || [],
-                }),
-              ]
-            );
+            return this.fb.array([
+              this.recursionBuildGroup(configs, {
+                validators: state?.data?.groupValidations || [],
+                asyncValidators: state?.data?.asyncGroupValidations || [],
+              }),
+            ]);
             break;
           }
 
@@ -135,4 +136,126 @@ export class DynamicFormBuilderService {
         break;
     }
   };
+
+  fromJsonArrayToConfigRecursion(json: JsonValue[], label?: string) {
+    const returnDefault = (json: any, i?: any) =>
+      ({
+        field: l ? l : '_',
+        ...(l
+          ? { label: `${l?.[0]?.toString()?.toUpperCase()}${l.substring(1)}` }
+          : {}),
+        type: 'FormArray',
+        data: {
+          formControlConfig: {
+            field: '_',
+            type: 'Default',
+            data: {
+              inputType: {
+                string: 'text',
+                number: 'number',
+                bigint: 'number',
+                boolean: 'checkbox',
+              }?.[typeof json?.[0]],
+            },
+          },
+        } as FormArrayData<{ _: string }>,
+      } as DynamicFormControl);
+    const l = label;
+    switch (typeof json?.[0]) {
+      case 'string':
+      case 'number':
+      case 'bigint':
+      case 'boolean': {
+        return returnDefault(json);
+        break;
+      }
+      case 'object': {
+        if (Array.isArray(json?.[0])) {
+          return {
+            field: l ? l : '_',
+            ...(l
+              ? {
+                  label: `${l?.[0]?.toString()?.toUpperCase()}${l.substring(
+                    1
+                  )}`,
+                }
+              : {}),
+            type: 'FormArray',
+            data: {
+              formArrayConfig: this.fromJsonArrayToConfigRecursion(json?.[0]),
+            } as FormArrayData<{ _: string[] }>,
+          } as DynamicFormControl;
+        } else if (json?.[0] !== null) {
+          return {
+            field: l ? l : '_',
+            ...(l
+              ? {
+                  label: `${l?.[0]?.toString()?.toUpperCase()}${l.substring(
+                    1
+                  )}`,
+                }
+              : {}),
+            type: 'FormArray',
+            data: {
+              formGroupConfig: this.fromJsonToConfigRecursion(json?.[0]),
+            } as FormArrayData<{ _: object }>,
+          } as DynamicFormControl;
+        } else {
+          return returnDefault(json);
+        }
+        break;
+      }
+      default:
+        break;
+    }
+  }
+
+  fromJsonToConfigRecursion(json: JsonObject) {
+    // debugger;
+    const returnDefault = (json: any, i: any) =>
+      ({
+        field: i,
+        type: 'Default',
+        value: json?.[i],
+        label: `${i?.[0]?.toString()?.toUpperCase()}${i.substring(1)}`,
+        data: {
+          inputType: {
+            string: 'text',
+            number: 'number',
+            bigint: 'number',
+            boolean: 'checkbox',
+          }?.[typeof json?.[i]],
+        },
+      } as DynamicFormControl);
+    return Object.keys(json || {})?.map((i) => {
+      switch (typeof json?.[i]) {
+        case 'string':
+        case 'number':
+        case 'bigint':
+        case 'boolean': {
+          return returnDefault(json, i);
+          break;
+        }
+        case 'object': {
+          if (Array.isArray(json?.[i])) {
+            return this.fromJsonArrayToConfigRecursion(json?.[i] as any, i);
+          } else if (json?.[i] !== null) {
+            return {
+              field: i,
+              label: `${i?.[0]?.toString()?.toUpperCase()}${i.substring(1)}`,
+              type: 'FormGroup',
+              data: {
+                formConfig: this.fromJsonToConfigRecursion(json?.[i] as any),
+              } as FormGroupData<{ _: object }>,
+            } as DynamicFormControl;
+          } else {
+            return returnDefault(json, i);
+          }
+          break;
+        }
+        default:
+          break;
+      }
+    });
+  }
 }
--- libs/front/main-app-testing/src/lib/pages/forms/forms.component.ts
+++ libs/front/main-app-testing/src/lib/pages/forms/forms.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, Validators } from '@angular/forms';
-import { DynamicFormControl } from '@fnx-nx/front/dynamic-forms';
+import {
+  DynamicFormBuilderService,
+  DynamicFormControl,
+} from '@fnx-nx/front/dynamic-forms';
 import { User } from '@fnx-nx/api-interfaces';
 import { firstValueFrom, timer } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
@@ -11,7 +14,11 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./forms.component.scss'],
 })
 export class FormsComponent implements OnInit {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private dfb: DynamicFormBuilderService
+  ) {}
+  @ViewChild('formComponent', { static: false }) public formComponent: any;
   group = new FormGroup({});
   config: DynamicFormControl<any>[];
   /** = [
@@ -69,23 +76,315 @@ export class FormsComponent implements OnInit {
         rows: 5,
       },
     };
+  dy2: DynamicFormControl<any> =
+    // { DynamicFormConfigurationObject: string, '_': string }
+    {
+      type: 'Default',
+      field: 'DynamicFormObjectValue',
+      label: 'Any Valid JSON',
+      placeholder: `{
+        "job": {
+            "title": "Developer",
+            "salary": "22",
+            "coworkers": [
+                {
+                    "name": "omri",
+                    "phoneNumber": "1"
+                },
+                {
+                    "name": "oren",
+                    "phoneNumber": "2"
+                }
+            ]
+        },
+        "matrix": [
+            [
+                "1",
+                "2",
+                "3",
+                null
+            ],
+            [
+                "2",
+                "4"
+            ]
+        ],
+        "roles": [
+            "10",
+            "20",
+            "30"
+        ],
+        "friends": [
+            {
+                "name": "nik",
+                "phoneNumber": "1",
+                "friends": [
+                    {
+                        "name": "zalina",
+                        "phoneNumber": "2",
+                        "friends": [
+                            {
+                                "name": "alon",
+                                "phoneNumber": "3"
+                            },
+                            {
+                                "name": "anabel",
+                                "phoneNumber": "4"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "ortal",
+                        "phoneNumber": "5",
+                        "friends": [
+                            {
+                                "name": "ziv",
+                                "phoneNumber": "6"
+                            },
+                            {
+                                "name": "adi",
+                                "phoneNumber": "7"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ],
+        "age": "24",
+        "name": "Mor Bargig"
+    }`,
+      validation: [Validators.required, Validators.minLength(10)],
+      errorMessages: {
+        required: 'Required',
+        minlength: 'minlength',
+      },
+      data: {
+        rows: 5,
+      },
+    };
   hello$ = this.http.get<DynamicFormControl<User>[]>('/api/helloForm');
   formOnSubmit(event: any) {
     alert(JSON.stringify(event));
   }
   clickServer() {
-    firstValueFrom(this.hello$).then((x) => {
-      this.config = null;
-      firstValueFrom(timer(0)).then(() => {
-        this.config = x;
-      });
-    });
+    // firstValueFrom(this.hello$).then((x) => {
+    //   this.config = null;
+    //   firstValueFrom(timer(0))
+    //     .then(() => {
+    //       this.config = x;
+    //     })
+    //     .catch(() =>
+    //       alert(`something want wrong!
+    //       server is not available at the time`)
+    //     );
+    // });
   }
   ngOnInit(): void {
-    firstValueFrom(this.hello$).then((x) => {
-      this.config = x;
-      // console.log(this.dfb.recursionBuildForm(x[0]));
-    });
+    this.config = [
+      {
+        field: 'job',
+        label: 'Job',
+        type: 'FormGroup',
+        data: {
+          formConfig: [
+            { field: 'title', label: 'Title' },
+            {
+              field: 'salary',
+              label: 'Salary',
+            },
+            {
+              type: 'FormArray',
+              field: 'coworkers',
+              label: 'Coworkers',
+              data: {
+                formGroupConfig: [
+                  {
+                    type: 'Default',
+                    field: 'name',
+                    label: 'Name',
+                  },
+                  {
+                    type: 'Default',
+                    field: 'phoneNumber',
+                    label: 'Phone',
+                    data: {
+                      inputType: 'tel',
+                    },
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      },
+      {
+        field: 'matrix',
+        type: 'FormArray',
+        label: 'Matrix',
+        placeholder: 'Matrix',
+        data: {
+          formArrayConfig: {
+            field: '_',
+            type: 'FormArray',
+            label: 'Matrix',
+            placeholder: 'Matrix',
+            data: {
+              formControlConfig: {
+                field: '_',
+                type: 'Default',
+                label: 'Matrix',
+                placeholder: 'Matrix',
+              },
+            },
+          },
+        },
+      },
+      {
+        field: 'roles',
+        type: 'FormArray',
+        label: 'Roles',
+        placeholder: 'Roles',
+        data: {
+          formControlConfig: {
+            field: '_',
+            type: 'Default',
+            label: 'Role',
+            placeholder: 'Role',
+            data: {
+              // inputType: 'number',
+              // title$: of(),
+            },
+          },
+        },
+      },
+      {
+        field: 'friends',
+        type: 'FormArray',
+        // onChange: ({ currentValue }) =>
+        //   currentValue[0].friends[0].friends[0].phoneNumber,
+        label: 'Friends List',
+        data: {
+          formGroupConfig: [
+            {
+              type: 'Default',
+              field: 'name',
+              label: 'Name',
+              // onChange: ({currentValue}) => null,
+            },
+            {
+              type: 'Default',
+              field: 'phoneNumber',
+              label: 'Phone',
+              data: {
+                inputType: 'tel',
+              },
+              // onChange: ({currentValue}) => null,
+            },
+            {
+              type: 'FormArray',
+              field: 'friends',
+              label: 'Friend Friends List',
+              data: {
+                formGroupConfig: [
+                  {
+                    type: 'Default',
+                    field: 'name',
+                    label: 'Name',
+                  },
+                  {
+                    type: 'Default',
+                    field: 'phoneNumber',
+                    label: 'Phone',
+                    data: {
+                      inputType: 'tel',
+                    },
+                  },
+                  {
+                    type: 'FormArray',
+                    field: 'friends',
+                    label: 'Friend Friends List',
+                    data: {
+                      formGroupConfig: [
+                        {
+                          type: 'Default',
+                          field: 'name',
+                          label: 'Name',
+                        },
+                        {
+                          type: 'Default',
+                          field: 'phoneNumber',
+                          label: 'Phone',
+                          data: {
+                            inputType: 'tel',
+                          },
+                        },
+                      ],
+                    },
+                  },
+                ],
+              },
+              // onChange: ({currentValue}) => null,
+            },
+          ],
+        },
+      },
+      {
+        field: 'age',
+        type: 'Default',
+        label: 'Age',
+        placeholder: 'Age',
+        data: {
+          inputType: 'number',
+          // title$: of(),
+        },
+      },
+      {
+        field: 'name',
+        type: 'Default',
+        label: 'Name',
+        placeholder: 'Name',
+        data: {
+          inputType: 'text',
+          // title$: of(),
+        },
+      },
+    ] as any;
+    console.log(this.config);
+    console.log([
+      {
+        type: 'Default',
+        field: 'username',
+        label: 'UserName',
+        placeholder: 'UserName',
+      },
+      {
+        type: 'Default',
+        field: 'password',
+        label: 'Password',
+        placeholder: 'Password',
+        // validation: [Validators.required, Validators.maxLength(10)],
+        // validation: ['Validators.required'],
+        data: { inputType: 'password' },
+      },
+      {
+        type: 'Default',
+        field: 'date',
+        label: 'Date',
+        placeholder: 'Date',
+        data: { inputType: 'datetime-local' },
+      },
+      {
+        type: 'Default',
+        field: 'description',
+        label: 'Description',
+        placeholder: 'Description',
+        data: { rows: 5 },
+      },
+    ]);
+    // firstValueFrom(this.hello$).then((x) => {
+    //   this.config = x;
+    //   // console.log(this.dfb.recursionBuildForm(x[0]));
+    // });
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
   }
@@ -101,6 +400,21 @@ export class FormsComponent implements OnInit {
           )()
         )
       );
+    });
+  }
+  click2() {
+    this.config = null;
+    firstValueFrom(timer(0)).then(() => {
+      this.config = this.dfb.fromJsonToConfigRecursion(
+        JSON.parse(
+          JSON.stringify(
+            new Function(
+              'return (' + this.group.getRawValue().DynamicFormObjectValue + ')'
+            )()
+          )
+        )
+      ) as any;
+      console.log(this.config);
     });
   }
 }
