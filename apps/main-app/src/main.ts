48ab32503d883357e953096cff9ac32357888403
Mor Bargig <morbargig@gmail.com>
2025-02-08 19:35:44 +0200

    update workspace for angular 19

--- apps/main-app/src/main.ts
+++ apps/main-app/src/main.ts
@@ -1,4 +1,5 @@
-import { enableProdMode } from '@angular/core';
+import 'zone.js'
+import { enableProdMode,provideZoneChangeDetection } from '@angular/core';
 import { environment } from './environments/environment';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
@@ -13,6 +14,8 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(ROUTES), // Configure the router with your routes
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptorsFromDi()),
+    provideZoneChangeDetection({ eventCoalescing: true })
   ],
 }).catch((err) => console.error(err));
+
