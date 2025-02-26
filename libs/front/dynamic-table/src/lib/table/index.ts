3f4af75de98aa5875f1e63fd5883b45a73d32c00
Mor Bargig <morbargig@gmail.com>
2025-02-26 16:11:04 +0000

    more fixes

--- libs/front/dynamic-table/src/lib/table/index.ts
+++ libs/front/dynamic-table/src/lib/table/index.ts
@@ -1,10 +1,6 @@
 export * from './cell-components';
 export * from './directives';
-export {
-  tableBodyStylesObj,
-  TableComponent,
-  tableElStyleObj,
-} from './table.component';
+export * from './table.component';
 export * from './core/pipes';
+export * from './styles/styles';
 export * from './components/table-filters/lazy-load-event.type';
-
