71eb4b59caa5cfd403b7c196c0ca19081955ab2f
Mor Bargig <morbargig@gmail.com>
2025-03-10 06:35:59 +0000

    fix css

--- apps/main-app/src/test-setup.ts
+++ apps/main-app/src/test-setup.ts
@@ -1,6 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+import { TranslateModuleConfigProvider } from '@softbar-ngx-translate';
 import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
 
 setupZoneTestEnv({
   errorOnUnknownElements: true,
   errorOnUnknownProperties: true,
+});
+
+TestBed.configureTestingModule({
+  providers: [
+    TranslateModuleConfigProvider({
+      moduleType: 'root',
+      translationsChunks: null,
+    }),
+  ],
 });
